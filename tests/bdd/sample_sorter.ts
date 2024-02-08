ce0546f766bfea67a9b4472fa3db3768f6b970d2
PaulinaCiesielska <111356315+PaulinaCiesielska@users.noreply.github.com>
2024-02-08 06:11:11 +0100

    Co-authored-by: PaCisdev <67173086+PaCisdev@users.noreply.github.com>

---
+++ tests/bdd/sample_sorter.ts
@@ -0,0 +1,61 @@
+import { Given, When, Then, DataTable } from '@badeball/cypress-cucumber-preprocessor';
+
+Given(`multiple patient profiles exist`, () => {
+    // [Given] Sets up the initial state of the system.
+});
+
+When(`a test tube is associated with each patient`, () => {
+    // [When] Describes the action or event that triggers the scenario.
+});
+
+Then(`each test tube should contain material specific to a single patient`, () => {
+    // [Then] Describes the expected outcome or result of the scenario.
+});
+
+Given(`multiple patient profiles with different ages exist`, () => {
+    // [Given] Sets up the initial state of the system.
+});
+
+When(`test tubes are assigned to racks`, () => {
+    // [When] Describes the action or event that triggers the scenario.
+});
+
+Then(`patients of the same age should not be placed in the same rack`, () => {
+    // [Then] Describes the expected outcome or result of the scenario.
+});
+
+Given(`patient profiles associated with different companies exist`, () => {
+    // [Given] Sets up the initial state of the system.
+});
+
+Then(`patients working in the same company should not be placed in the same rack`, () => {
+    // [Then] Describes the expected outcome or result of the scenario.
+});
+
+Given(`patient profiles residing in different city districts exist`, () => {
+    // [Given] Sets up the initial state of the system.
+});
+
+Then(`patients living in the same city district should not be placed in the same rack`, () => {
+    // [Then] Describes the expected outcome or result of the scenario.
+});
+
+Given(`patient profiles with different vision defects exist`, () => {
+    // [Given] Sets up the initial state of the system.
+});
+
+Then(`patients with the same vision defect should not be placed in the same rack`, () => {
+    // [Then] Describes the expected outcome or result of the scenario.
+});
+
+Given(`a limited number of racks is available`, () => {
+    // [Given] Sets up the initial state of the system.
+});
+
+Given(`a large number of patient profiles exist`, () => {
+    // [Given] Sets up the initial state of the system.
+});
+
+Then(`the application should handle rack limitations appropriately`, () => {
+    // [Then] Describes the expected outcome or result of the scenario.
+});
